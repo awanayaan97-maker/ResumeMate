681249e48205a313e7a65292bd89ce32fcca1876
wajidkhan99 <peshwears@gmail.com>
2026-05-20 20:57:55 +0500

    kr do bhiua

--- src/app.js
+++ src/app.js
@@ -6,7 +6,25 @@ const cors = require("cors");
 const resumeRouter = require("./Routes/resumeRouter");
 const AiRouter = require("./Routes/AiRoutes");
 
-app.use(cors());
+// app.use(cors());
+
+// Purane app.use(cors()) ki jagah yeh lagao:
+app.use(cors({
+    origin: '*', // Har origin ko allow karo
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version', 'Authorization'],
+    credentials: true
+}));
+
+// Pre-flight requests (OPTIONS) ko Express level par handle karne ke liye:
+app.options('*', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,PATCH,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
+    res.sendStatus(200);
+});
+
+app.use(express.json());
 app.use(express.json());
 
 connectDB();
